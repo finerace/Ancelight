1c3b655570f564c03d4cb4cc803d8d82c4ef47ed
FineRace <saveley99@gmail.com>
2022-10-27 00:35:28 +0500

    Made settings menu - 5%

---
+++ Assets/Scripts/UI/Common/SimpleScripts/ScrollbarMouseScrolling.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ScrollbarMouseScrolling : MonoBehaviour,IScrollHandler
+{
+    [SerializeField] private Scrollbar targetScrollbar;
+    
+    [Space] 
+    [SerializeField] private float scrollPower = 1;
+    [SerializeField] private bool invertScrollPower = true;
+    
+    
+    public void OnScroll(PointerEventData eventData)
+    {
+        if(targetScrollbar.gameObject.activeSelf == false)
+            return;
+        
+        var realScrollPower = eventData.scrollDelta.y;
+        var scrollPowerSmoothing = 100 / scrollPower;
+
+        if (invertScrollPower)
+            realScrollPower = -realScrollPower;
+            
+        ScrollScrollbar(realScrollPower / scrollPowerSmoothing);
+        
+        void ScrollScrollbar(float scrollingPower)
+        {
+            var scrollbarValue = targetScrollbar.value;
+            
+            var isScrollingPowerUnused =
+                (scrollbarValue <= 0 && scrollingPower < 0) ||
+                (scrollbarValue >= 1 && scrollingPower > 0);
+            
+            if(isScrollingPowerUnused)
+                return;
+
+            targetScrollbar.value += scrollingPower;
+        }
+    }
+
+}
